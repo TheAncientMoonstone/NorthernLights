f97ddedd885c38aa64dd9c78db8456e092683584
TheAncientMoonstone <richo91_Richardson@hotmail.com>
2016-01-26 01:11:17 +1100

    Basic signup and login features without the need for a Facebook account added - needs to be tested

--- NorthernLights/ParseLogInViewController.swift
+++ NorthernLights/ParseLogInViewController.swift
@@ -43,5 +43,41 @@ class ParseLogInViewController: UIViewController, PFLogInViewControllerDelegate,
         
     }
     
-
+    func signUpViewController() {
+        // This allows for users to signup without to have a Facebook account.
+        let user = PFUser()
+        
+        user.username = "myUsername"
+        user.password = "myPassword"
+        user.email = "email@example.com"
+        user["phone"] = "04111-62558"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error{
+                let errorString = error.userInfo["error"] as? NSString
+                // Show the errorString somewhere and let the user try that again.
+            } else {
+                // Hooray! Let them use the app now!
+            }
+        }
+        
+    }
+    
+    func logInViewController() {
+        PFUser.logInWithUsernameInBackground("myname", password: "mypass") {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                // Do stuff after successful login.
+            } else {
+                // The login failed. Check error to see why.
+        }
+            let currentUser = PFUser.currentUser()
+            if currentUser != nil {
+                // Do stuff with the user
+            } else {
+                // Show the signup or login screen.
+            }
+        }
+    }
 }
